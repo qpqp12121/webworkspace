994e31dadc03c05599772159d77b9f786c3784be
qpqp12121 <qpqp12121@naver.com>
2024-02-06 08:04:04 +0900

    24.02.05(2)

--- homework/db/DDL.sql
+++ homework/db/DDL.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `t_users`( 
+CREATE TABLE `t_users` (
 	`user_no` INT AUTO_INCREMENT,
-    `user_id` VARCHAR(100) NOT NULL UNIQUE,
+    `user_id` VARCHAR(100) NOT NULL,
     `user_pwd` VARCHAR(100) NOT NULL,
     `user_name` VARCHAR(100) NOT NULL,
-    `user_gender` CHAR(1) CHECK(user_gender IN ('M', 'F')),
+    `user_gender` CHAR(1) CHECK( user_gender IN ('M', 'F') ),
     `user_age` INT,
     `join_date` DATE,
     PRIMARY KEY(`user_no`)
